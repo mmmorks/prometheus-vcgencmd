31168d38cb228c8e1387666e0d8386cdee51e27f
Karl Rink <karl@rink.us>
2021-06-14 20:02:29 -0700

    0.0.0-2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="prometheus-vcgencmd",
-    version="0.0.0-1",
+    version="0.0.0-2",
     author="Karl Rink",
     author_email="karl@rink.us",
     description="prometheus-vcgencmd",
